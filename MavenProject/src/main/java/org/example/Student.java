b34a5c672d63a1eace9e1a52bf1e2260eb1791fc
Harshal Bhalkar <92264308+harshb010@users.noreply.github.com>
2025-08-18 18:49:25 +0530

    Add files via upload

---
+++ MavenProject/src/main/java/org/example/Student.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+public class Student {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
